33b501a3a453d5f4e01e0153931b569c51d3fd07
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-06 16:51:54 +0700

    perf: improves code performance

---
+++ app/src/main/java/com/zakora/musicapp/data/AlbumData.java
@@ -0,0 +1,37 @@
+package com.zakora.musicapp.data;
+
+import com.zakora.musicapp.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AlbumData {
+    public static List<Map<String, Object>> albumList = new ArrayList<Map<String, Object>>(){{
+        add(new HashMap<String, Object>() {{
+            put("title", "Pajama Drive");
+            put("image", R.drawable.pajama_drive);
+        }});
+        add(new HashMap<String, Object>() {{
+            put("title", "Heavy Rotation");
+            put("image", R.drawable.heavy_rotation);
+        }});
+        add(new HashMap<String, Object>() {{
+            put("title", "Apricot Princess");
+            put("image", R.drawable.apricot_princess);
+        }});
+        add(new HashMap<String, Object>() {{
+            put("title", "Unorthodox Jukebox");
+            put("image", R.drawable.unorthodox_jukebox);
+        }});
+        add(new HashMap<String, Object>() {{
+            put("title", "Bintang Lima");
+            put("image", R.drawable.bintang_lima);
+        }});
+        add(new HashMap<String, Object>() {{
+            put("title", "Bcos U Will Never B Free");
+            put("image", R.drawable.pajama_drive);
+        }});
+    }};
+}
--- app/src/main/java/com/zakora/musicapp/ui/MainActivity.java
+++ app/src/main/java/com/zakora/musicapp/ui/MainActivity.java
@@ -9,15 +9,19 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.zakora.musicapp.R;
+import com.zakora.musicapp.data.AlbumData;
 import com.zakora.musicapp.model.Album;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     List<Album> albums = new ArrayList<Album>();
-    String albumList[] = {"Pajama Drive", "Apricot Princess", "Unorthodox Jukebox", "Bintang Lima", "Heavy Rotation", "bcos U Will Never B Free"};
-    int albumImages[] = {R.drawable.pajama_drive, R.drawable.apricot_princess, R.drawable.unorthodox_jukebox, R.drawable.bintang_lima,R.drawable.heavy_rotation, R.drawable.bcos_u};
+
+//    String albumList[] = {"Pajama Drive", "Apricot Princess", "Unorthodox Jukebox", "Bintang Lima", "Heavy Rotation", "bcos U Will Never B Free"};
+//    int albumImages[] = {R.drawable.pajama_drive, R.drawable.apricot_princess, R.drawable.unorthodox_jukebox, R.drawable.bintang_lima,R.drawable.heavy_rotation, R.drawable.bcos_u};
 
     LinearLayout linearLayout;
     @Override
@@ -25,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for(int i = 0 ; i < albumList.length; i++){
-            albums.add(new Album(albumList[i],albumImages[i]));
+        for(Map<String, Object> data: AlbumData.albumList){
+            String title = (String) data.get("title");
+            Integer imageInteger = (Integer) data.get("image");
+            int image = (imageInteger != null) ? imageInteger : 0;
+
+            albums.add(new Album(title,image));
         }
 
         RecyclerView recyclerView = findViewById(R.id.rv_recently_played);
